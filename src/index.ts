12459580c9eefd70cdef5b27f6a6c589a229d364
Mazinho <irismarandrade94@gmail.com>
2026-02-27 18:28:23 -0300

    chore: adiciona o fastify/swagger e fastify/swagger-ui no projeto

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
 
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import Fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
@@ -14,6 +17,27 @@ const app = Fastify({
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "API Treino Smart",
+      description: "API de treino",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "locahost",
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
 
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
